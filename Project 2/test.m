8ce3528847a6a8825b4c6a6b4445c2f3e813c0db
tonytonkovich <160249346+tonytonkovich@users.noreply.github.com>
2024-03-16 13:24:06 -0400

    New Plot

---
+++ Project 2/test.m
@@ -0,0 +1,32 @@
+close all
+
+mach = out.mach(:,2);
+cd = out.Cd(:,2);
+alt = out.Altitude(:,2);
+angle = out.Angle(:,2);
+
+[mach_sort, idxs] = sort(mach);
+cd_sort = cd(idxs);
+
+figure
+plot(mach_sort, cd_sort);
+
+[alt_sort, idxs] = sort(alt);
+cd_sort = cd(idxs);
+angle_sort = angle(idxs);
+
+figure
+plot(alt_sort, cd_sort, 'LineWidth',1);
+xlim([0, 70000]);
+grid on
+xlabel('Altitude [m]')
+ylabel('Coefficient of Drag [-]')
+title('Cd vs. Altitude')
+
+figure
+plot(alt_sort, angle_sort)
+grid on
+xlabel('Altitude [m]')
+ylabel('Turn Angle [deg]')
+title('Rocket Turn Angle vs. Altitude')
+
