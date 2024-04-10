8cb5deb7abab6a339c7454abcdec691829649b4d
tonytonkovich <tony.tonkovich21@gmail.com>
2024-04-10 13:55:50 -0400

    FIXED
    This one is closer. Still not perfect though

--- Project 4/Project4.m
+++ Project 4/Project4.m
@@ -15,19 +15,24 @@ tx0     = 0;
 thead0  = 0; 
 mv0     = 1.5*c;       
 mx0     = -5000;  
-my0     = 13000;  
+my0     = 12000;  
 launch_ang = 0;
 th0     = rad2deg(atan2((ty0-my0),(tx0-mx0)));
+vc0     = sqrt((mv0*cosd(launch_ang) + tv0*cosd(thead0))^2 + (mv0*sind(launch_ang) + tv0*sind(thead0))^2);
 
 % ODE Solver
-[t, x] = ode45(@(t,x) guide(t, x, dt, N), [0:dt:tSim],...
-        [tv0; tx0; ty0; thead0; mv0; mx0; my0; launch_ang; th0]);
+[t, x] = ode45(@(t,x) guide(t, x, dt, N), [0:dt:tSim],[tv0; tx0; ty0; thead0; mv0; mx0; my0; launch_ang; th0; vc0]);
  
-    
-% for i = 1:length(t)
-%     [~,R(i), vc(i), a(i)] = guide(t(i), x(i,:), vc0);
-% end
-% 
+
+
+% Plotting
+figure;
+plot(x(:,2), x(:,3))
+hold on
+plot(x(:,6), x(:,7))
+title('Flight Profile')
+saveas(gcf, 'Figures/P4Trajectories.png');
+
 
 
 function [dxdt] = guide(t, x, dt, N)
@@ -37,34 +42,35 @@ tdhead = x(4);
 tdx = x(1)*cosd(x(4));
 tdy = x(1)*sind(x(4));
 
-
 tx = x(2);
 ty = x(3);
 mx = x(6);
 my = x(7);
 X  = tx-mx;
 Y  = ty-my;
-R  = sqrt(X^2 -Y^2);
+R  = sqrt(X^2-Y^2);
 th = x(9);
-thn = atan2(Y,X);
-dth = thn/dt;
+thn = rad2deg(atan2(Y,X));
+dth = thn-th;
 
 tvx = tdx;
 tvy = tdy;
 mvx = x(5)*cosd(x(8));
 mvy = x(5)*sind(x(8));
-vc  = sqrt((mvx + tvx)^2 + (mvy + tvy)^2);
-a = N * dth * vc;
+vc = x(10);
+vcn  = sqrt((mvx + tvx)^2 + (mvy + tvy)^2);
+dvc = vcn-vc;
+a = N * deg2rad(dth) * vc
 
-mth = x(8) + a/10;
-mdth = mth/dt;
+mth = x(8);
+mdth = a;
 mdx = x(5)*cosd(mth);
 mdy = x(5)*sind(mth);
 
 mdv = 0;
 
 
-    dxdt = [tdv; tdx; tdy; tdhead; mdv; mdx; mdy; mdth; dth;];
+    dxdt = [tdv; tdx; tdy; tdhead; mdv; mdx; mdy; mdth; dth; dvc];
 end 
 
 
