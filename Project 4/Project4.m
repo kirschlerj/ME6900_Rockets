9cc68ad2226eb72a5608eea4f5b0e21af17ac746
tonytonkovich <tony.tonkovich21@gmail.com>
2024-04-11 13:53:47 -0400

    New Figs and Updates

--- Project 4/Project4.m
+++ Project 4/Project4.m
@@ -4,38 +4,64 @@ clear, clc, close all
 
 % Constants
 c = 343;
-dt = 0.1;
-tSim = 25;
-N = 0.7;
+tSim = 18.12;
+N = 3;
 
 % Initial Conditions
-tv0     = 250;     
+tv0     = 1.2*c;     
 ty0     = 13000;  
 tx0     = 0;      
 thead0  = 0; 
-mv0     = 500;       
+mv0     = 3*c;       
 mx0     = -5000;  
-my0     = 12000;  
+my0     = 26000;  
 launch_ang = 0;
 th0     = (atan2((ty0-my0),(tx0-mx0)));
+r0      = sqrt((tx0-mx0)^2 + (ty0-my0)^2);
 vc0     = sqrt((mv0*cosd(launch_ang) + tv0*cosd(thead0))^2 + (mv0*sind(launch_ang) + tv0*sind(thead0))^2);
 
 % ODE Solver
-[t, x] = ode45(@(t,x) guide(t, x, dt, N), [0 tSim],[tv0; tx0; ty0; thead0; mv0; mx0; my0; launch_ang; th0; vc0]);
+[t, x] = ode45(@(t,x) guide(t, x, N), [0 tSim],[tv0; tx0; ty0; thead0; mv0; mx0; my0; launch_ang; th0; vc0]);
  
-
+for i = 1:length(t)
+    [~, R(i), dth(i)] = guide(t(i), x(i,:), N);
+end 
 
 % Plotting
 figure;
 plot(x(:,2), x(:,3))
 hold on
 plot(x(:,6), x(:,7))
+xlabel('X Position [m]')
+ylabel('Y Position [m]')
 title('Flight Profile')
+legend('Target Position', 'Missile Position');
 saveas(gcf, 'Figures/P4Trajectories.png');
 
+figure;
+yyaxis left
+plot(t, R')
+ylabel('Distance [m]')
+yyaxis right
+plot(t, x(:,10))
+ylabel('Closing Velocity [m/s]');
+xlabel('Time [s]')
+title('Missile - Target Relations')
+saveas(gcf, 'Figures/P4MTRelations.png');
+
+figure;
+yyaxis left
+plot(t, rad2deg(x(:,9)))
+ylabel('Theta [deg]')
+yyaxis right
+plot(t, rad2deg(dth'))
+ylabel('Theta Dot [deg/s]');
+xlabel('Time [s]')
+title('Theta Relations')
+saveas(gcf, 'Figures/P4thetaRelations.png');
 
 
-function [dxdt] = guide(t, x, dt, N)
+function [dxdt, R, dth] = guide(t, x, N)
 
 tdv = 0;
 tdhead = x(4);
@@ -52,7 +78,7 @@ my = x(7);
 
 X  = tx-mx;
 Y  = ty-my;
-R  = sqrt(X^2-Y^2);
+R  = sqrt(X^2+Y^2);
 th = x(9);
 thn = (atan2(Y,X));
 dth = thn-th;
@@ -66,14 +92,10 @@ vcn  = sqrt((mvx + tvx)^2 + (mvy + tvy)^2);
 dvc = vcn-vc;
 a = N * (dth) * vc;
 
-
 mdth = a/10;
-
-
 mdv = 0;
 
-
-    dxdt = [tdv; tdx; tdy; tdhead; mdv; mdx; mdy; mdth; dth; dvc];
+dxdt = [tdv; tdx; tdy; tdhead; mdv; mdx; mdy; mdth; dth; dvc];
 end 
 
 
